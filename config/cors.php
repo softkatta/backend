37bad55b60e83b5e2d0075a07e0f2377856f86c4
softkatta <softkatta@gmail.com>
2026-07-17 07:49:02 +0530

    m

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,http://127.0.0.1:3000,https://softkatta.com,https://www.softkatta.com'))),
+    'allowed_origins' => array_filter(explode(',', env(
+        'CORS_ALLOWED_ORIGINS',
+        'http://localhost:5173,http://127.0.0.1:5173,http://localhost:3000,http://127.0.0.1:3000,https://softkatta.in,https://www.softkatta.in'
+    ))),
 
     'allowed_origins_patterns' => [],
 
