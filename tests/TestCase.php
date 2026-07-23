09dbd4120a2b7eed2b1e03bde662871279272498
softkatta <softkatta@gmail.com>
2026-07-23 11:50:26 +0530

    m

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,10 +8,17 @@ abstract class TestCase extends BaseTestCase
 {
     protected function setUp(): void
     {
-        parent::setUp();
+        $dbConnection = (string) (
+            $_ENV['DB_CONNECTION']
+            ?? $_SERVER['DB_CONNECTION']
+            ?? getenv('DB_CONNECTION')
+            ?? 'sqlite'
+        );
 
-        if (config('database.default') === 'sqlite' && ! extension_loaded('pdo_sqlite')) {
+        if ($dbConnection === 'sqlite' && ! extension_loaded('pdo_sqlite')) {
             $this->markTestSkipped('pdo_sqlite extension is required for sqlite in-memory tests.');
         }
+
+        parent::setUp();
     }
 }
